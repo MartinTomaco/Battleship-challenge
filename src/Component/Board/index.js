9681f990de5dc299b24da3b268f1a09e267bc3d8
Martin Gonzalez <marttingonzalez@gmail.com>
2022-01-17 09:51:03 -0300

    First commit with board layout

---
+++ src/Component/Board/index.js
@@ -0,0 +1,143 @@
+import React from "react";
+import Square from "../Square";
+import "./Board.css";
+
+export default function Board() {
+  return (
+    <div>
+      <div className="status">{}</div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </div>
+  );
+}
---
+++ src/Component/Game/index.js
@@ -0,0 +1,17 @@
+import "./Game.css";
+
+import React from 'react'
+import Board from "../Board";
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        Hola dfdggd
+        <Board/>
+      </div>
+    </div>
+  )
+}
+
+
