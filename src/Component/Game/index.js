f7ccbed2c257ab7c6a8478a284b4b87eb703e9be
Martin Gonzalez <marttingonzalez@gmail.com>
2022-01-22 11:49:48 -0300

    Adds available ships list

--- src/Component/Game/index.js
+++ src/Component/Game/index.js
@@ -1,17 +1,30 @@
 import "./Game.css";
 
-import React from 'react'
+import React from "react";
 import Board from "../Board";
 
 export default function Game() {
   return (
     <div className="game">
-      <div className="game-board">
-        Hola dfdggd
-        <Board/>
-      </div>
+      <section className="main-container">
+        <section className="top-container">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="right-panel">
+            <input className="name-input" placeholder="Player name"></input>
+            <button className="startGame-button"> Start Game</button>
+          </div>
+        </section>
+        <section className="bottom-container">
+        <b>Available ships:</b>
+        <li>carrier of 4 spaces</li>
+        <li>cruiser  of 3 spaces</li>
+        <li>cruiser  of 3 spaces</li>
+        <li>cruiser  of 3 spaces</li>
+        <li>submarine of 2 spaces</li>
+        </section>
+      </section>
     </div>
-  )
+  );
 }
-
-
